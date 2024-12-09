cfb41cbd753705a814913e96868dee3c96e5d7b2
phamminhteky1 <20014236@mvsm.org>
2024-12-09 09:38:48 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+console.log("Welcome to my page's debug console!")
+  console.log("Note: This page uses Bootstrap using CDN and sone internet resources brought here with link. If you downloaded this page but can't use this, please connect to the Internet.")
